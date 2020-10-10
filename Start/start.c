f4966554332eddeaba794b17fb2f8586c23a8417
Carlos Leite <carlosleite.dev@gmail.com>
2020-10-10 16:55:42 +0000

    Add a Start folder

---
+++ Start/start.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+
+int start(void) {
+  printf("first one\n");
+  return 0;
+}
--- main.c
+++ main.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 
 int main(void) {
-  printf("Hello World\n");
+  printf("Welcome to my Repository\n");
   return 0;
 }
